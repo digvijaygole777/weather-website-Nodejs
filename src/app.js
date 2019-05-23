9853a9c143e3e068e37c940d045d43e927a8400c
Digvijay Gole <digvijaygole777@outlook.com>
2019-05-23 14:13:42 +0530

    Add consol.log call

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const hbs=require('hbs');
 const geocode=require('./utils/geocode');
 const forecast=require('./utils/forecast');
 
+console.log('Git commit');
+
 const app=express();
 
 const publicDirPath=path.join(__dirname,'../public');
